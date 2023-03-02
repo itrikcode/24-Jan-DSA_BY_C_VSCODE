2915c482227516eb00a5711f350e9aa9029d4cd6
vivek kumar <kumarviveklohaman@gmail.com>
2023-03-02 19:53:26 +0530

    test

---
+++ sum_of_element_in_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+int main() {
+   
+   int a,b,c;
+  printf("1. Add ");
+   printf("2. sub ");
+    printf("3. mul ");
+     printf("chooce option ");
+     scanf("%d",&a);
+
+     switch (a)
+     {
+     case 1:
+        printf("Add tow Number ");
+        scanf("%d,%d ",&b,&c);
+        break;
+      break;
+     
+     default:
+      break;
+     }
+  return 0;
+}
